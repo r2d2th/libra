fc012a3559047b521cee717d7cabd911fc9c831a
r2d2th <aandb.tg@gmail.com>
2019-08-26 22:57:46 +0300

    chore: removed outdated routes

--- app/routes.py
+++ app/routes.py
@@ -9,28 +9,3 @@ import requests
 def index():
     return render_template('index.html', user='Docker')
 
-
-@app.route('/logout')
-def logout():
-    return redirect(url_for('index'))
-
-
-@app.route('/city')
-def city():
-    """
-    Gets some data
-    and then renders it.
-    """
-    try:
-        req = requests.get("http://127.0.0.1:5001/cities")
-    except requests.exceptions.ConnectionError:
-        raise ServiceUnavailable("The Cities service is unavailable.")
-
-    if req.status_code == 404:
-        raise NotFound("No cities were found")
-
-    data = req.json()
-    cities = data['cities']
-    return render_template('city.html', title='City Info', user='City Docker', cities=cities)
-
-
