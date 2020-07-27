172cb0329ec896c8f23c46d11d6ab3919770292e
r2d2th <aandb.tg@gmail.com>
2020-07-27 19:07:39 +0300

    feat: Add client initial

---
+++ app/client/index.js
@@ -0,0 +1,5 @@
+import AppContainer        from './containers/appContainer';
+import { componentRender } from './components/basestore';
+import '../static/main.css';
+
+componentRender(AppContainer,   'header');
